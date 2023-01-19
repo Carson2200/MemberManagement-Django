f89974ad409bc2de1defe4cc68e66d4856308854
carson2200 <carsonosore78@gmail.com>
2023-01-19 14:27:58 +0300

    First Commit of Member Management System in Python Django

--- trialtwo.py
+++ trialtwo.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class New_member(models.model):
+
+class New_member(models.Model):
     objects = models.manager
     firstName = models.CharField(max_length=250)
     lastName = models.CharField(max_length=250)
@@ -8,5 +9,6 @@ class New_member(models.model):
     phone = models.CharField(max_length=10)
 
     userName = models.CharField(max_length=20)
+
     def __str__(self):
         return f'New_member({self.firstName}, {self.lastName}, {self.email},{self.userName}, {self.phone})'
